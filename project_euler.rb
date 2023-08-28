9e83f778235687d38ce42b570d0d3b18f1344112
Funto <funto.afuwape@gmail.com>
2023-08-28 10:47:32 +0100

    Solve Project Euler problem 2

--- project_euler.rb
+++ project_euler.rb
@@ -8,3 +8,11 @@ def multiples_of_3_or_5(n, array=[])
   end
   multiples_of_3_or_5(n, array)
 end
+
+def even_fibonacci_nums(array=[0,1])
+  if array.any? { |element| element >= 4_000_000 }
+    return array.keep_if {|element| element % 2 == 0}.sum
+  end
+  array << array.last + array[array.size - 2]
+  even_fibonacci_nums(array)
+end
